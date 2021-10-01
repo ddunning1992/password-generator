3ff9db31ab4cc55a4ad0ec192f2d13a45f4864cb
Dunnhalr <88796836+ddunning1992@users.noreply.github.com>
2021-10-01 15:39:57 -0500

    got character set options working

--- generator.js
+++ generator.js
@@ -1,4 +1,6 @@
 // Assignment code here
+var symbols = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 var getPasswordLength = function() {
   var length = "";
 
@@ -8,26 +10,59 @@ var getPasswordLength = function() {
   return length;
 };
 
+ var useLowerCase = function() {
+  var charset1 = "";
+  var lCase = prompt("Would you like to use lowercase letters? Type 'true' or 'false'.");
+  
+    if (lCase === "true") {
+        charset1 = "abcdefghijklmnopqrstuvwxyz";
+    }
+    return charset1;
+};
+
 var useUpperCase = function() {
-  var charset = "";
+  var charset2 = "";
+  var uCase = prompt("Would you like to use uppercase letters? Type 'true' or 'false'.");
 
-  var uCasePrompt = window.prompt("Would you like to use uppercase letters? type 'true' or 'false'.");
-
-  if (uCasePrompt === true) {
-      charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (uCase === "true") {
+      charset2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  return charset;
+  return charset2;
+};
+
+var useNumbers = function() {
+  var charset3 = "";
+  var numbers = prompt("Would you like to use numbers? Type 'true' or 'false'.");
+
+  if (numbers === "true") {
+      charset3 = 0123456789;
+  }
+  return charset3;
+};
+
+var useSpecialCharacters = function() {
+  var charset4 = "";
+  var characters = prompt("Would you like to use special characters? Type 'true' or 'false'.");
+
+  if (characters === "true") {
+    charset4 = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  }
+  return charset4;
 };
 
 function generatePassword() {
-  var length = getPasswordLength(),
-      charset = "abcdefghijklmnopqrstuvwxyz" + useUpperCase();
+  var length = getPasswordLength();
+      charset = useLowerCase() + useUpperCase() + useNumbers() + useSpecialCharacters();
       retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
 };
+
+var finalCharset = function () {
+
+}
 
 
 // Get references to the #generate element
